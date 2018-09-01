efa70d14e6cb13fb4822317d1314fed3daf59892
zhyjiang <zhyjiang@mobvoi.com>
2018-09-01 14:32:45 +0800

    [first project] maven

---
+++ _1/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2018 Mobvoi Inc. All Rights Reserved.
+ */
+
+/**
+ * @author zhyjiang, zhyjiang@mobvoi.com
+ * @date Sep 01, 2018
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+
+}
